38fa6cb9ee1e59b1bb7e6f2c3516640da4be8aed
Herli Khoury <herli.kh@gmail.com>
2022-09-13 11:41:39 -0300

    Imagens_estrutura_dataBase

--- js/database.js
+++ js/database.js
@@ -1,64 +1,228 @@
-// Banco de dados dos produtos
-
-const data = [
+const swords = [
   {
     id: 1,
-    img: "../img/jaqueta.svg",
-    nameItem: "Lightweight Jacket",
+    img: "../img/swords/sword_0.jpeg",
+    nameItem: "Espada do Rei",
     description:
-      "Adicione um pouco de energia ao seu guarda-roupa de inverno com esta jaqueta vibrante...",
-    value: 100,
+      "Seja belo e imponente ao mesmo tempo, a Espada do Rei garante os dois.",
+    value: 300,
     addCart: "Adicionar ao carrinho",
-    tag: ["Camisetas"],
+    tag: ["Espadas"]
   },
   {
     id: 2,
-    img: "../img/gorro.svg",
-    nameItem: "Black Hat",
+    img: "../img/swords/sword_1.jpeg",
+    nameItem: "Sabre Barba Negra",
     description:
-      "O gorro Next.js chegou! Esta beldade bordada tem um ajuste confortável que garante que...",
-    value: 100,
+      "Um marujo bem preparado sempre está com seu sabre na cintura. O sabre Bar...",
+    value: 200,
     addCart: "Adicionar ao carrinho",
-    tag: ["Acessórios"],
+    tag: ["Espadas"]
   },
   {
     id: 3,
-    img: "../img/mascara.svg",
-    nameItem: "Mask",
+    img: "../img/swords/sword_2.jpeg",
+    nameItem: "Ferroada",
     description:
-      "Esta máscara facial durável é feita de duas camadas de tecido tratado e possui presilhas...",
-    value: 40,
+      "A espada de Bilbo e Frodo. Se ilumina com uma luz azul forte na presença de orcs.",
+    value: 500,
     addCart: "Adicionar ao carrinho",
-    tag: ["Acessórios"],
+    tag: ["Espadas"]
   },
   {
     id: 4,
-    img: "../img/camiseta_preta.svg",
-    nameItem: "T-Shirt",
+    img: "../img/swords/sword_3.jpeg",
+    nameItem: "Glamdrin",
     description:
-      "Esta t-shirt é imprescindível no seu guarda-roupa, combinando o caimento intemporal de...",
-    value: 100,
+      "A espada de Gandalf, forjada pelos Valar. You cannot wish for a finner blade.",
+    value: 1000,
     addCart: "Adicionar ao carrinho",
-    tag: ["Camisetas"],
+    tag: ["Espadas"]
   },
   {
     id: 5,
-    img: "../img/camiseta_branca.svg",
-    nameItem: "Short-Sleeve T-Shirt",
+    img: "../img/swords/sword_4.jpeg",
+    nameItem: "Inquisidora",
     description:
-      "Agora você encontrou a camiseta básica do seu guarda-roupa. É feito de um mais grosso...",
-    value: 100,
+      "Abençoada pelo papa no século V. Livre se dos pagãos com esta lâmina abençoada.",
+    value: 700,
     addCart: "Adicionar ao carrinho",
-    tag: ["Camisetas"],
+    tag: ["Espadas"]
   },
   {
     id: 6,
-    img: "../img/moletom.svg",
-    nameItem: "Champion Packable Jacket",
+    img: "../img/swords/sword_5.jpeg",
+    nameItem: "Espada do Guardião",
     description:
-      "Proteja-se dos elementos com esta jaqueta embalável Champion. Esta jaqueta de poliést...",
-    value: 100,
+      "Grande e resistente a ferrugem. A Espada do Guardião foi feita para te todo tipo...",
+    value: 600,
     addCart: "Adicionar ao carrinho",
-    tag: ["Camisetas"],
+    tag: ["Espadas"]
   },
+  {
+    id: 7,
+    img: "../img/swords/sword_6.jpeg",
+    nameItem: "Katana",
+    description:
+      "Esta espada super resistente consegue cortar uma folha e um homem ao meio.",
+    value: 800,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Espadas"]
+  },
+  {
+    id: 8,
+    img: "../img/swords/sword_7.jpeg",
+    nameItem: "Facão do explorador",
+    description:
+      "Usada como ferramenta assim como arma. Suprirá qualquer necessidade em uma aventura.",
+    value: 150,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Espadas"]
+  },
+  {
+    id: 9,
+    img: "../img/swords/sword_8.jpeg",
+    nameItem: "Espada do Conde",
+    description:
+      "Empunhada em duas mãos a espada do conde é expecialista em derrubar alvos que se aproximam.",
+    value: 600,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Espadas"]
+  },
+  {
+    id: 10,
+    img: "../img/swords/sword_9.jpeg",
+    nameItem: "Espada do Principe",
+    description:
+      "Não muito testada em batalha, mas linda de morrer.",
+    value: 700,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Espadas"]
+  }       
 ];
+
+const axes = [
+  {
+    id: 11,
+    img: "../img/axes/axe_0.jpeg",
+    nameItem: "Machado de caminhada",
+    description:
+      "Machado de caminhada de Gimli, usado como apoio e como arma.",
+    value: 500,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Machados"]
+  },
+  {
+    id: 12,
+    img: "../img/axes/axe_1.jpeg",
+    nameItem: "Machado barbado",
+    description:
+      "Machado barbado de Gimli. Leve e resistente, corta madeira e orcs.",
+    value: 300,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Machados"]
+  },
+  {
+    id: 13,
+    img: "../img/axes/axe_2.jpeg",
+    nameItem: "Machado de batalha",
+    description:
+      "Machado de batalha de Gimli, usado em batalhas para destruir muitos orcs.",
+    value: 900,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Machados"]
+  },
+  {
+    id: 14,
+    img: "../img/axes/axe_3.jpeg",
+    nameItem: "Machado viking",
+    description:
+      "Forjado em aço bretão muito resistente e com detalhes de runas em relevo.",
+    value: 600,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Machados"]
+  },
+  {
+    id: 15,
+    img: "../img/axes/axe_4.jpeg",
+    nameItem: "Machado viking plus",
+    description:
+      "Forjado em aço bretão muito resistente e com detalhes de runas em relevo.",
+    value: 600,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Machados"]
+  },
+  {
+    id: 16,
+    img: "../img/axes/axe_5.jpeg",
+    nameItem: "Machado de lenha",
+    description:
+      "Machado robusto de 2 mãos, forjado em aço. Inclui bainha.",
+    value: 500,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Machados"]
+  },
+  {
+    id: 17,
+    img: "../img/axes/axe_6.jpeg",
+    nameItem: "Machado de Kratos",
+    description:
+      "Machado Leviatã, o machado mágico de Kratos o Deus da guerra.",
+    value: 1500,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Machados"]
+  }   
+]
+
+const knives = [
+  {
+    id: 18,
+    img: "../img/knives/knive_0.jpeg",
+    nameItem: "Faca de caça I",
+    description:
+      "Lâmina 15 cm, cabo preto, forjada em aço. Inclui bainha preta.",
+    value: 50,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Facas"]
+  },
+  {
+    id: 19,
+    img: "../img/knives/knive_1.jpeg",
+    nameItem: "Faca de caça II",
+    description:
+      "Lâmina 10 cm, cabo de madeira, forjada em aço. Inclui bainha.",
+    value: 60,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Facas"]
+  },
+  {
+    id: 20,
+    img: "../img/knives/knive_2.jpeg",
+    nameItem: "Faca de caça GVDV",
+    description:
+      "Lâmina 10 cm, cabo de madeira, forjada em aço. Inclui bainha em couro.",
+    value: 70,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Facas"]
+  },
+  {
+    id: 21,
+    img: "../img/knives/knive_3.jpeg",
+    nameItem: "Faca Bowie",
+    description:
+      "Lâmina 20 cm, forjada em aço. Boa para caça, pesca, churrasco.",
+    value: 150,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Facas"]
+  },
+  {
+    id: 22,
+    img: "../img/knives/knive_4.jpeg",
+    nameItem: "Faca em damasco",
+    description:
+      "Lâmina 7 cm, forjada em aço damasco. Boa para desossa e limpar peixes.",
+    value: 50,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Facas"]
+  }
+]
